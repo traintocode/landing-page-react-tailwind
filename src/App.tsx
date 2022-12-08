a14ba15787dce03d909e8b503d826c63d3e7ffef
James Charlesworth <jcharlesworthuk@gmail.com>
2022-12-08 22:06:09 +0000

    Add lottie files and tailwind in

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Lottie from "lottie-react";
+import cactusAnimation from "./cactus.json";
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const lottieRef = useRef();
+  useEffect( () => {
+    if (lottieRef) setTimeout(() => (lottieRef.current as any).pause(), 1500);
+  }, [lottieRef]);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+  return <main className="">
+    {/* <!--Nav--> */}
+    <div className="w-full container mx-auto p-6">
+
+      <div className="w-full flex items-center justify-between">
+        <a className="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+          {/* <img className="h-12 fill-current text-indigo-600 pr-2" src="cactus.svg" /> */}
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+        <div className="flex w-1/2 justify-end">
+          <div className="">
+            <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+          </div>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+
+    <Lottie animationData={cactusAnimation} lottieRef={lottieRef as any} />
+
+    <a href="https://www.vecteezy.com/free-vector/logo-svg">Logo Svg Vectors by Vecteezy</a>
+  </main>
 }
 
 export default App
