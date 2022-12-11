aed16e48a4ac9a1e1d3c074f8107f4c728392a84
James Charlesworth <jcharlesworthuk@gmail.com>
2022-12-11 11:30:23 +0000

    Added marketing copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Lottie, { LottieRefCurrentProps } from "lottie-react";
 import cactusAnimation from "./cactus.json";
 import Feature from './Feature';
 
 function App() {
-  const [count, setCount] = useState(0)
   const lottieRef = useRef<LottieRefCurrentProps>(null);
   useEffect(() => {
-    // if (lottieRef) setTimeout(() => (lottieRef.current as any).pause(), 1500);
     console.log('lottieRef change ', lottieRef);
     if (lottieRef.current) {
       console.log('lottieRef goToAndStop ');
@@ -37,19 +34,19 @@ function App() {
 
     <div className="mx-auto container p-6 lg:flex lg:items-center lg:justify-between">
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-        <span className="block">Ready to dive in?</span>
-        <span className="block text-amber-500">Start your free trial today.</span>
+        <span className="block">Slow and Steady Growth</span>
+        <span className="block text-amber-500">With minimum effort.</span>
       </h2>
       <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-        <Lottie animationData={cactusAnimation} lottieRef={lottieRef as any} loop={false} />
+        <Lottie animationData={cactusAnimation} lottieRef={lottieRef as any} loop={false} className="lottie-cactus" />
       </div>
     </div>
 
     <div className="container mx-auto bg-white py-12 sm:py-16 lg:py-20">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="sm:text-center">
-          <h2 className="text-lg font-semibold leading-8 text-amber-600">Transactions</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better way to send money</p>
+          <h2 className="text-lg font-semibold leading-8 text-amber-600">Features</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need in one place</p>
           <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
             Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
             accusamus quisquam.
@@ -58,23 +55,23 @@ function App() {
 
         <div className="mt-20 max-w-lg sm:mx-auto md:max-w-none">
           <div className="grid grid-cols-1 gap-y-16 md:grid-cols-2 md:gap-x-12 md:gap-y-16">
-            <Feature name="Grows Quickly" description='Grows incredibly fast'>
+            <Feature name="Minimum Investment" description='Cactus costs very litle to get started'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
               </svg>
             </Feature>
-            <Feature name="Grows Quickly" description='Grows incredibly fast'>
+            <Feature name="Steady Growth" description='Watch your Cactus grow a little each year'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
               </svg>
             </Feature>
-            <Feature name="Wireless Operation" description='Grows incredibly fast'>
+            <Feature name="Wireless Operation" description='Minimum power consumption makes Cactus great for the environment'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.412 15.655L9.75 21.75l3.745-4.012M9.257 13.5H3.75l2.659-2.849m2.048-2.194L14.25 2.25 12 10.5h8.25l-4.707 5.043M8.457 8.457L3 3m5.457 5.457l7.086 7.086m0 0L21 21" />
               </svg>
             </Feature>
 
-            <Feature name="Security First" description='Grows incredibly fast'>
+            <Feature name="Security First" description='With multiple layers of protection to keep intruders at bay'>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
               </svg>
@@ -84,11 +81,12 @@ function App() {
       </div>
     </div>
 
+    {/* https://flowbite.com/blocks/marketing/pricing/ */}
     <section className="container mx-auto bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for homes and offices</h2>
+          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Cactus delivers long term value to any workspace, conservatory or living area.  You can even install it in the gym.</p>
         </div>
         <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
           {/*Pricing Card */}
@@ -216,36 +214,31 @@ function App() {
             <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor" />
           </svg>
           <blockquote>
-            <p className="text-2xl font-medium text-gray-900 dark:text-white">"Flowbite is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application."</p>
+            <p className="text-2xl font-medium text-gray-900 dark:text-white">"A cactus doesn't live in the desert because it likes the desert; it lives there because the desert hasn't killed it yet."</p>
           </blockquote>
           <figcaption className="flex items-center justify-center mt-6 space-x-3">
-            {/* <img className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture"> */}
             <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-              <div className="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
-              <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+              <div className="pr-3 font-medium text-gray-900 dark:text-white">Hope Jahren</div>
+              <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">American Geochemist</div>
             </div>
           </figcaption>
         </figure>
       </div>
     </section>
 
-
-
+    {/* https://flowbite.com/docs/components/footer/ */}
     <footer className="container mx-auto p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://traintocode.com/" className="hover:underline">traintocode.com</a>. Released under MIT license.
       </span>
       <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+          <a href="https://github.com/traintocode/landing-page-react-tailwind" className="mr-4 hover:underline md:mr-6 ">GitHub Source</a>
         </li>
         <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          Additional Components from <a href="https://flowbite.com/docs/getting-started/license/" className="mr-4 hover:underline md:mr-6">Flowbite</a>
         </li>
         <li>
-          <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
-        </li>
-        <li>
-          <a href="#" className="hover:underline">Contact</a>
+          Image by <a href="https://lottiefiles.com/19570-cactus-3" className="mr-4 hover:underline md:mr-6">Tom Faber</a>
         </li>
       </ul>
     </footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      // you can configure the container to be centered
+      center: true,
+
+      // or have default horizontal padding
+      padding: '1rem',
+
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '600px',
+        md: '700px',
+        lg: '900px',
+        xl: '1100px',
+        '2xl': '1200px',
+      },
+    },
   },
   plugins: [],
 }
